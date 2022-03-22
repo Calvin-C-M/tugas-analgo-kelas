5ad9dd2280498265d738d50ca24091be64234448
Calvin-C-M <montolalu28@gmail.com>
2022-03-22 08:51:33 +0700

    Fixed count sort

--- countSort.cpp
+++ countSort.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
+int maxElement(vector<int> data) {
+    int value=0;
+    for(int d: data) {
+        if(value < d) 
+            value=d;
+    }
+    return value;
+}
+
+int minElement(vector<int> data) {
+    int value=maxElement(data);
+    for(int d: data) {
+        if(value > d) 
+            value=d;
+    }
+    return value;
+}
+
 void countSort(vector<int> &data) {
-    int max=*max_element(data.begin(),data.end());
-    int min=*min_element(data.begin(),data.end());
+    int max=maxElement(data);
+    int min=minElement(data);
     int range=max-min+1;
 
     vector<int> count(range), output(data.size());
     for(int d: data) 
         count[d-min]++;
 
-    for(int i=0; i<count.size(); i++) 
+    for(int i=1; i<count.size(); i++) 
         count[i]+=count[i-1];
     
     for(int i=data.size()-1; i>=0; i--) {
-        output[count[data[i]-min-1]]=data[i];
+        output[count[data[i]-min]-1]=data[i];
         count[data[i]-min]--;
     }
 
