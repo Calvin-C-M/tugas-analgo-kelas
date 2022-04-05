8ee95f636f85db35e986f805bddd9dca99faa618
Calvin-C-M <montolalu28@gmail.com>
2022-04-05 08:16:23 +0700

    Added heap sort algorithm

---
+++ heapSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void heapify(vector<int> &data, int n, int i) {
+    int max=i;
+    int left=2*i+1;
+    int right=2*i+2;
+
+    if(left < n && data.at(left) > data.at(max)) 
+        max=left;
+
+    if(right < n && data.at(right) > data.at(max)) 
+        max=right;
+
+    if(max != i) {
+        swap(data.at(i),data.at(max));
+        heapify(data,n,max);
+    }
+}
+
+void heapSort(vector<int> &data, int n) {
+    for(int i=n/2-1; i >= 0; i--)   
+        heapify(data,n,i);
+    for(int i=n-1; i > 0; i--) {
+        swap(data.at(0),data.at(i));
+        heapify(data,i,0);
+    }
+}
+
+void printData(vector<int> data) {
+    for(int d: data) 
+        cout << d << "  ";
+}
+
+int main() {
+
+}
