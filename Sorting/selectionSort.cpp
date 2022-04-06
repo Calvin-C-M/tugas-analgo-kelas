936ca1a54a2acad32b7951031085f12d7f4047b2
Calvin-C-M <montolalu28@gmail.com>
2022-04-06 22:34:40 +0700

    Added selection sort algorithm

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,28 @@
+#include "../header.hpp"
+
+namespace SelectionSort {
+    void min(vector<int> &data) {
+        int minIndex=0;
+
+        for(int i=0; i<data.size()-1; i++) {
+            minIndex=i;
+            for(int j=i+1; j<data.size(); j++) 
+                if(data.at(j) < data.at(minIndex))
+                    minIndex=j;
+            
+            swap(data.at(minIndex),data.at(i));
+        }
+    }
+
+    void max(vector<int> &data) {
+        int maxIndex=0;
+        for(int i=0; i<data.size()-1; i++) {
+            maxIndex=i;
+            for(int j=i+1; j<data.size(); j++) 
+                if(data.at(j) > data.at(maxIndex))
+                    maxIndex=j;
+            
+            swap(data.at(maxIndex),data.at(i));
+        }
+    }
+}
