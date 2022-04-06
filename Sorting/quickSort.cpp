a36bb837ac2011d688d312c93eec4cde599ae3c9
Calvin-C-M <montolalu28@gmail.com>
2022-04-06 22:25:38 +0700

    Added quick sort algorithm

---
+++ Sorting/quickSort.cpp
@@ -0,0 +1,28 @@
+#include "../header.hpp"
+
+int partition(vector<int> &data, int low, int high) {
+    int pivot=data.at(high);
+    int i=low-1;
+
+    for(int j=low; j<=high; j++) {
+        if(data.at(j) < pivot) {
+            i++;
+            swap(data.at(i),data.at(j));
+        }
+    }
+    swap(data.at(i+1),data.at(high));
+    return i+1;
+}
+
+void quickSort(vector<int> &data, int low, int high) {
+    if(low < high) {
+        int pi=partition(data,low,high);
+
+        quickSort(data,low,pi-1);
+        quickSort(data,pi+1,high);
+    }
+}
+
+int main() {
+
+}
